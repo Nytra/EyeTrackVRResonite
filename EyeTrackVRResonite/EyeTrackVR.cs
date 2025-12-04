b2f8492212478b29eb19c459979960f8377690aa
Nytra <14206961+Nytra@users.noreply.github.com>
2025-12-04 00:21:45 +0000

    Change link to point to galister's repo

--- EyeTrackVRResonite/EyeTrackVR.cs
+++ EyeTrackVRResonite/EyeTrackVR.cs
@@ -12,7 +12,7 @@ namespace EyeTrackVRResonite
         public override string Name => "EyeTrackVRResonite";
         public override string Author => "PLYSHKA + dfgHiatus + Nytra";
         public override string Version => "3.0.0";
-        public override string Link => "https://github.com/Nytra/EyeTrackVRResonite";
+        public override string Link => "https://github.com/galister/EyeTrackVRResonite";
 
         public override void OnEngineInit()
         {
